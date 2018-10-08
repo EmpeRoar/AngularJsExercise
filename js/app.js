91f628afee85938ff0cca245e1e6ca86d30afddf
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-08 17:13:07 +0800

    Injector

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("Julius Bacosa");
+var myAppx = angular.module('myApp',[]);
+
+myAppx.controller('mainController', function($scope){
+    console.log($scope);
+});
+
+var searchPeople = function(firstName, lastName, age, occupation){
+    return 'Jane Doe';
+};
+
+
+console.log(angular.injector().annotate(searchPeople));
