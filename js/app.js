d84885a10cb0cd68bc8280040805a1134ec5905c
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-08 18:26:52 +0800

    Dependency Injection

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-var myAppx = angular.module('myApp',[]);
+var myAppx = angular.module('myApp',['ngMessages','ngResource']);
 
-myAppx.controller('mainController', function($scope){
-    console.log($scope);
-});
+myAppx.controller('mainController', [
+    '$scope', '$log', '$filter', '$resource',
+    function($scope, $log, $filter, $resource){
+        console.log($scope);
+    
+        $log.log("Hello");
+        $log.info("This is some information");
+        $log.warn("Warning!");
+        $log.debug("Some debug information");
+        $log.error("This is an error!");
+    
+        $scope.name = 'John';
+        $scope.formattedname = $filter('uppercase')($scope.name);
+        $log.info($scope.name);
+        $log.info($scope.formattedname);
+    
+        console.log($resource);
+    
+    }
+]);
 
 var searchPeople = function(firstName, lastName, age, occupation){
     return 'Jane Doe';
