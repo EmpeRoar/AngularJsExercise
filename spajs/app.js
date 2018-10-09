3ce01e3a5549adddcab10e0d407f92dd5a299c5e
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-09 08:26:35 +0800

    services

--- spajs/app.js
+++ spajs/app.js
@@ -16,12 +16,42 @@ myAppx.config(function($routeProvider){
         })
 });
 
-myAppx.controller('mainController', ['$scope', '$log', function($scope, $log){
+myAppx.service('nameService', function(){
 
+    var self = this;
+    this.name = 'John Doe';
+
+    this.namelength = function(){
+
+        return self.name.length;
+
+    };
+
+});
+
+myAppx.controller('mainController', ['$scope', '$log', 'nameService',  function($scope, $log, nameService){
+    
+    $scope.name = nameService.name;
+
+    $scope.$watch('name', function(){
+        nameService.name = $scope.name;
+    });
+
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
 }]);
 
-myAppx.controller('secondController', ['$scope','$log','$routeParams', function($scope, $log, $routeParams){
+myAppx.controller('secondController', ['$scope','$log','$routeParams', 'nameService', function($scope, $log, $routeParams, nameService){
     $scope.num = $routeParams.num || 1;
+
+    
+    $scope.$watch('name', function(){
+        nameService.name = $scope.name;
+    });
+    
+    $scope.name = nameService.name;
+
+
 }]);
 
 
